720370fdbd879940b694a9aba6e2e6e8592b3d00
Arlo Filley <104267514+ArloFilley@users.noreply.github.com>
2022-05-10 19:44:58 +0100

    Create Fighter.java

---
+++ Fighter.java
@@ -0,0 +1,9 @@
+// this creates the call fighter which is used both for enemies and the player
+// during the fight(); function
+public class Fighter{
+  int health = 0;
+  int attack = 0;
+  int defence = 0;
+  int experience = 0;
+  int level = 0;
+}
